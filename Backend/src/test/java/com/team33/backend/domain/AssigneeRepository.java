695ea2c4cd6058985363e76134d521dc86891916
Jun <92818747+devjun10@users.noreply.github.com>
2022-06-16 20:39:41 +0900

    Revert "feature: 공통 예외처리 기본 devjun10/issue-tracker#13"
    This reverts commit 874d02406e5985abe0f6e6d3b58aa63d7e04bad2.

--- Backend/src/test/java/com/team33/backend/domain/AssigneeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team33.backend.domain;
-
-import com.team33.backend.issue.domain.Assignee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssigneeRepository extends JpaRepository<Assignee, Long> {
-}
--- Backend/src/test/java/com/team33/backend/domain/IssueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team33.backend.domain;
-
-import com.team33.backend.issue.domain.Issue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssueRepository extends JpaRepository<Issue, Long> {
-}
