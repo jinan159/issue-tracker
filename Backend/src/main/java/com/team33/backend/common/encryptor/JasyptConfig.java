e507f1edec17b69e58b179a2622867ccf9b45d54
jinan159 <wlsdhks0423@naver.com>
2022-07-01 01:51:58 +0900

    refactor: 설정파일 암호화 bean 추가

---
+++ Backend/src/main/java/com/team33/backend/common/encryptor/JasyptConfig.java
@@ -0,0 +1,30 @@
+package com.team33.backend.common.encryptor;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    public static final String ENV_JASYPT_KEY = "JASYPT_KEY";
+
+    @Bean
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(System.getenv(ENV_JASYPT_KEY));
+        config.setAlgorithm("PBEWithMD5AndTripleDES");
+        config.setKeyObtentionIterations(1000);
+        config.setPoolSize(1);
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
